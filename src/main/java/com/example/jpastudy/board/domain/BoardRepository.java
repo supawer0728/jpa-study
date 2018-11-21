195efacec39ad009a6e0efe115d56adc01ed24ff
Jihoon.Jeon <jihoon.jeon@nhnent.com>
2018-11-21 21:29:34 +0900

    불필요한 메서드 제거

--- src/main/java/com/example/jpastudy/board/domain/BoardRepository.java
+++ src/main/java/com/example/jpastudy/board/domain/BoardRepository.java
@@ -1,6 +1,5 @@
 package com.example.jpastudy.board.domain;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,8 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findAllByTitleContains(String title, Pageable pageable);
-
-    List<Board> findAllByWriterStartsWith(String writer);
-
-    List<Board> findAllByTitleAndWriter(String title, String writer);
 }
